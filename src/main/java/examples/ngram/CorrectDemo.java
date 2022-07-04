dbe433c4e3ef506957597b31dbe758738ce3fac1
seastar <2229029156@qq.com>
2022-07-04 20:50:57 +0800

    update examples

--- src/main/java/examples/ngram/CorrectDemo.java
+++ src/main/java/examples/ngram/CorrectDemo.java
@@ -1,33 +1,32 @@
-package examples;
+package examples.ngram;
 
 import org.apache.commons.lang3.StringUtils;
-import sy.core.spelling.Detector;
+import sy.core.spelling.Corrector;
 
 import java.util.Scanner;
 
 /**
  * @author sy
- * @date 2022/2/5 20:42
+ * @date 2022/2/21 20:21
  */
-public class DetectDemo {
+public class CorrectDemo {
     public static void main(String[] args) {
         /**
          * 我的喉咙发炎了要买点阿莫细林吉
          * 少先队员因该为老人让坐
          */
-        Detector detector = new Detector();
+        Corrector corrector = new Corrector();
         String sentence;
         while (true) {
             System.out.println("Please input a sentence:");
             Scanner scanner = new Scanner(System.in);
             sentence = scanner.next();
-            System.out.println(detector.detect(sentence));
+            System.out.println(corrector.correct(sentence));
             if(StringUtils.equals(sentence, "exit") || StringUtils.equals(sentence, "quit")) {
                 System.out.println("exit or quit!");
                 break;
             }
         }
+
     }
-
 }
-
