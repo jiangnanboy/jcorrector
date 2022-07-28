e5e215b398fcd218f67b7921d6107e929689cfd1
seastar <2229029156@qq.com>
2022-07-28 21:20:41 +0800

    add gec check

---
+++ src/main/java/examples/gec/GecDemo.java
@@ -0,0 +1,29 @@
+package examples.gec;
+
+import org.apache.commons.lang3.StringUtils;
+import sy.core.gec.GecCheck;
+import util.PropertiesReader;
+
+import java.util.Scanner;
+
+/**
+ * @author YanShi
+ * @date 2022/7/28 21:15
+ */
+public class GecDemo {
+    public static void main(String...args) {
+        String templatePath = GecCheck.class.getClassLoader().getResource(PropertiesReader.get("template")).getPath().replaceFirst("/", "");
+        GecCheck gecRun = new GecCheck();
+        gecRun.init(templatePath);
+        String sentence;
+        while (true) {
+            System.out.println("Please input a sentence:");
+            Scanner scanner = new Scanner(System.in);
+            sentence = scanner.next();
+            String infoStr = gecRun.checkCorrect(sentence);
+            if(StringUtils.isNotBlank(infoStr)) {
+                System.out.println(infoStr);
+            }
+        }
+    }
+}
